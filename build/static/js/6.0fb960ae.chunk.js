5ba3429ffb124ccfbb2b94869d6766ebf56ad9f0
Kyrylo Moskalenko <krllmsk@gmail.com>
2021-12-08 19:59:15 +0200

    fixed styles in modal

--- build/static/js/6.0fb960ae.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpcontacts=this.webpackJsonpcontacts||[]).push([[6],{255:function(t,n,c){t.exports={ContactsApp:"HomeView_ContactsApp__1R6CN"}},261:function(t,n,c){"use strict";c.r(n),c.d(n,"default",(function(){return p}));var s=c(255),e=c.n(s),o=c(2);function p(){return Object(o.jsx)("div",{className:e.a.ContactsApp,children:Object(o.jsx)("h2",{children:"Contacts App"})})}}}]);
-//# sourceMappingURL=6.0fb960ae.chunk.js.map
